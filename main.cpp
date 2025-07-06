558a9a59464c62dd0fe9a6f0bc1846ca38e61434
Ziad Eliwa <ziadeliwa@aucegypt.edu>
2025-07-06 22:16:14 +0300

    Splitted Tasks and Write Routes for Login and Sign Up

--- main.cpp
+++ main.cpp
@@ -1,7 +1,27 @@
-#include <crow.h>
-using namespace std;
+#include "include/crow_all.h"
+#include <unordered_map>
+#include <openssl/sha.h>
+#include <sstream>
+#include <iomanip>
 
 int main()
 {
-    return 0;
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/login").methods("POST"_method)([](const crow::request& req){
+        auto body = crow::json::load(req.body);
+        if(!body) return crow::response(400,"Invalid JSON");
+        
+        return crow::response(200, "login successful");
+    });
+
+    CROW_ROUTE(app, "/signup").methods("POST"_method)([](const crow::request& req){
+        auto body = crow::json::load(req.body);
+        if(!body) return crow::response(400,"Invalid JSON");
+
+        return crow::response(200, "Signup successful");
+
+    });
+
+    app.port(18080).multithreaded().run();
 }
