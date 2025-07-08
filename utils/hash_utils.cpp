0e036b1d3fc948b993da74cea6cddffe160d4034
Ali-Helal <alyhelal20@gmail.com>
2025-07-09 00:01:12 +0300

    Implements password hashing function in hash_utils.cpp

---
+++ utils/hash_utils.cpp
@@ -0,0 +1,21 @@
+#include "hash_utils.h"
+#include <sodium.h>
+#include <stdexcept>
+
+
+
+std::string hashed(const std::string &pass) {
+        char hashed_pass[crypto_pwhash_STRBYTES];
+    
+        if (crypto_pwhash_str(
+            hashed_pass,
+            pass.c_str(), pass.size(),
+            crypto_pwhash_OPSLIMIT_INTERACTIVE,
+            crypto_pwhash_MEMLIMIT_INTERACTIVE) != 0) {
+        // Out of memory or other error
+        throw std::runtime_error("Password hashing failed");
+        }
+        
+        return std::string(hashed_pass);
+}
+
