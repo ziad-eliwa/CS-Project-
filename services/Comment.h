e70513586c698c4954ff56a9dd4ffcbff73c78f3
Ali Hosny <alihosny7ul@gmail.com>
2025-07-21 20:59:10 +0300

    finished modifying comment

--- services/Comment.h
+++ services/Comment.h
@@ -4,12 +4,14 @@
 #include <ctime>
 
 class Comment {
-public:
+
     int id;
     int post_id;
     std::string user_name;
     std::string content;
     std::time_t created_at;
+
+    public:
 
     Comment();
     Comment(int id, int post_id, const std::string& user_name, const std::string& content, std::time_t created_at);
@@ -23,5 +25,8 @@ public:
 
     // Setters
     void setContent(const std::string& new_content);
+    void setId(int new_id);
+    void setPostId(int new_post_id);
+    void setUserName(const std::string& new_user_name);
     void setCreatedAt(std::time_t new_created_at);
 };
