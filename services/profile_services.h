eb27a9d1fa5cd6885c12e6fc2b6a115acb75a015
Ali-Helal <alyhelal20@gmail.com>
2025-07-21 21:48:23 +0300

    Implemented user profile handler and user profile services

--- services/profile_services.h
+++ services/profile_services.h
@@ -1,4 +1,33 @@
 #ifndef PROFILE_SERVICES_H
 #define PROFILE_SERVICES_H
 
+#pragma once
+
+#include <string>
+#include <sqlite3.h>
+
+struct UserProfile {
+    std::string display_name;
+    std::string avatar_url;
+    std::string bio;
+    std::string location;
+    std::string website;
+};
+
+class ProfileService {
+public:
+
+    static bool getProfile(sqlite3* db, const std::string& username, UserProfile& profile);
+
+
+    static bool updateProfile(sqlite3* db, const std::string& username, const UserProfile& newProfile);
+
+
+    static bool createDefaultProfile(sqlite3* db, const std::string& username);
+};
+
+
+
+
+
 #endif
